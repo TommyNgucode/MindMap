efec3c5347050c2173117a87e8704c26fd7f003c
tommyngucode <s4093392@student.rmit.edu.au>
2025-06-23 03:05:48 +1000

    Updated making node

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let nodes = []
 let edges = []
 
-const interactiveBoard = document.getElementsByClassName("canvas")
-
+const interactiveBoard = document.getElementById("interactiveBoard")
+const options = document.getElementById('options')
 
 interactiveBoard.addEventListener("click", (e) =>{
+    console.log(e.clientX, e.clientY)
+    let el = "<div>"
     
+    el += "<input type='text'  placeholder='Enter' >"
+    options.style.position = "absolute"
+    options.style.top = e.clientY + "px"
+    options.style.left = e.clientX + "px"
+    options.style.display = "block"
+    options.style.opacity = "1"
+
+    el += "</div>"
+    options.innerHTML = el
 })
+
+
+// checks if node already exist at x and y
+// returns boolean 
+// function nodeExist()
+
