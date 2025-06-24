da68fea7c7d7786f0ee5e90f84fd7c13487c051a
tommyngucode <s4093392@student.rmit.edu.au>
2025-06-25 00:48:19 +1000

    Updated node connected

--- script.js
+++ script.js
@@ -14,15 +14,18 @@ interactiveBoard.addEventListener("click", (e) =>{
     container.style.cursor = "move"
     container.style.transitionDuration = 0
     
+    
     const input = document.createElement("input")
     container.appendChild(input)
     
     input.readOnly = true
     
+    // To type double click node
     input.addEventListener("dblclick", ()=>{
         input.readOnly = false
     })
 
+    // Press enter to lock text into node
     input.addEventListener("keydown", (e)=> {
 
         const keyPressed = e.key
@@ -34,12 +37,19 @@ interactiveBoard.addEventListener("click", (e) =>{
     })
     options.appendChild(container)
 
+    // Delays opacity of node for transition to occur
     setTimeout(() => {
         container.style.opacity = 1
     }, 10)
 
-    
-    // drag node
+    // Add node to nodes list
+    nodes.push({
+        el: container,
+        x: e.clientX + rect.width / 2,
+        y: e.clientY + rect.height / 2
+    })
+
+    // Drag/ Move node
     let offsetX = 0;
     let offsetY = 0;
 
@@ -51,9 +61,53 @@ interactiveBoard.addEventListener("click", (e) =>{
         document.addEventListener("mouseup", mouseUp);
     });
     
+    // get all rendering content of canvas
+    const ctx = interactiveBoard.getContext("2d")
+    const rect = interactiveBoard.getBoundingClientRect()
+    
     function mouseMove(e) {
         container.style.left = (e.clientX - offsetX) + "px";
         container.style.top = (e.clientY - offsetY) + "px";
+
+        // Detect nearby nodes
+
+        const currX = parseFloat(container.style.left = (e.clientX - offsetX) + "px")
+        const currY = parseFloat(container.style.top = (e.clientY - offsetY) + "px")
+
+
+        ctx.clearRect(0, 0, interactiveBoard.width, interactiveBoard.height); // clear canvas to avoid overdraw
+
+        nodes.forEach((node) =>{
+            const nodeX = node.x - rect.left
+            const nodeY = node.y - rect.top
+
+            
+            // calculate distance formula
+            const distance = Math.sqrt(((nodeX - currX) ** 2) + ((nodeY - currY) ** 2))
+
+            // Connect nodes
+            if (distance <= 70) {
+
+                const edge = `${currX},${currY}-${nodeX},${nodeY}`
+                const reverseEdge = `${nodeX},${nodeY}-${currX},${currY}`
+
+                // if edge doesnt exist add
+                if (!edges.includes(edge) && !edges.includes(reverseEdge)) {
+                    edges.push(edge)
+                    edges.push(reverseEdge)
+
+                    // draw line between
+                    ctx.lineWidth = 2
+                    ctx.beginPath()
+                    ctx.moveTo(currX, currY)
+                    ctx.lineTo(nodeX, nodeY)
+                    ctx.stroke()
+
+                }
+
+            }
+
+        })
     }
 
     function mouseUp() {
@@ -62,22 +116,11 @@ interactiveBoard.addEventListener("click", (e) =>{
         
     }
 
-        
+
+    function connectNode() {
+
+    }
 
 })
 
-
-
-// when mousedown
-// track mouse movement
-// when mouse relaesd (mourseup) stop it
-
-
-
-
-
-
-// checks if node already exist at x and y
-// returns boolean 
-// function nodeExist()
 
